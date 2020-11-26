80ca0293f8d43b50ccc6628b0d6d649f409a13d0
Robert Finkley <rfinkley@gmail.com>
2020-11-26 13:46:23 -0500

    first commit

---
+++ server/express.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import compress from 'compression';
+import cors from 'cors';
+import helmet from 'helmet';
+import Template from '../template.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compress());
+app.use(helmet());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).send(Template());
+});
+
+export default app;
---
+++ template.js
@@ -0,0 +1,13 @@
+export default () => {
+  return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MERN Skeleton</title>
+    </head>
+    <body>
+        <div id="root">Hello World</div>
+    </body>
+    </html>`;
+};
