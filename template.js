7cc74dfc514a15864be5ab47be53dee1d4c1c92c
Robert Finkley <rfinkley@gmail.com>
2020-12-05 17:56:18 -0500

    added Roboto and Material Icons

--- template.js
+++ template.js
@@ -4,6 +4,8 @@ export default () => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
         <title>MERN Skeleton</title>
     </head>
     <body>
