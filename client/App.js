1d0dedaf391f5fb629162354668dd31bc39aadc2
Robert Finkley <rfinkley@gmail.com>
2020-12-05 22:00:43 -0500

    added hot reloading

--- client/App.js
+++ client/App.js
@@ -3,6 +3,7 @@ import MainRouter from './MainRouter';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles';
 import theme from './theme';
+import { hot } from 'react-hot-loader';
 
 const App = () => {
   return (
@@ -14,4 +15,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default hot(module)(App);
