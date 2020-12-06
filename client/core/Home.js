6144b7304b59946163160549c6afc0178f4237c9
Robert Finkley <rfinkley@gmail.com>
2020-12-05 22:36:46 -0500

    added jss styles

--- client/core/Home.js
+++ client/core/Home.js
@@ -5,3 +5,20 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import girlSmiling from '../assets/images/girlSmiling.jpg';
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: theme.spacing(5),
+  },
+  title: {
+    padding: `${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(
+      2
+    )}px`,
+    color: theme.palette.openTitle,
+  },
+  media: {
+    minHeight: 400,
+  },
+}));
