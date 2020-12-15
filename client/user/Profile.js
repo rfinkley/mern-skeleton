3716ca42d9fb9752cf1885b30496059829719122
Robert Finkley <rfinkley@gmail.com>
2020-12-15 18:18:04 -0500

    code to check for authentication and load user

--- client/user/Profile.js
+++ client/user/Profile.js
@@ -30,6 +30,33 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Profile = () => {};
+function Profile({ match }) {
+  const classes = useStyles();
+  const [user, setUser] = useState({});
+  const [redirectToSignin, setRedirectToSignin] = useState(false);
+  const jwt = auth.isAuthenticated();
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    read(
+      {
+        userId: match.params.userId,
+      },
+      { t: jwt.token },
+      signal
+    ).then((data) => {
+      if (data && data.error) {
+        setRedirectToSignin(true);
+      } else {
+        setUser(data);
+      }
+    });
+    return function cleanup() {
+      abortController.abort();
+    };
+  }, [match.params.userId]);
+}
 
 export default Profile;
