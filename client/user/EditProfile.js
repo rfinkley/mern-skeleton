d4842e4b3d5601e148f05ea52e4ef18cc749c440
Robert Finkley <rfinkley82@gmail.com>
2020-12-17 18:57:36 -0500

    added handleChange function

--- client/user/EditProfile.js
+++ client/user/EditProfile.js
@@ -10,6 +10,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import auth from './../auth/auth-helper';
 import { read, update } from './api-user.js';
 import { Redirect } from 'react-router-dom';
+import { values } from 'lodash';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -92,6 +93,10 @@ function EditProfile({ match }) {
         setValues({ ...values, userId: data._id, redirectToProfile: true });
       }
     });
+  };
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
   };
 }
 
