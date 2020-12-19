6d998de3b848cb9a58925213623581368d046089
Robert Finkley <rfinkley82@gmail.com>
2020-12-18 22:14:23 -0500

    completed DeleteUser component

--- client/user/DeleteUser.js
+++ client/user/DeleteUser.js
@@ -12,6 +12,66 @@ import auth from './../auth/auth-helper';
 import { remove } from './api-user.js';
 import { Redirect } from 'react-router-dom';
 
-function DeleteUser(props) {}
+function DeleteUser(props) {
+  const [open, setOpen] = useState(false);
+  const [redirect, setRedirect] = useState(false);
+  const jwt = auth.isAuthenticated();
+  const clickButton = () => {
+    setOpen(true);
+  };
+
+  const deleteAccount = () => {
+    remove(
+      {
+        userId: props.userId,
+      },
+      { t: jwt.token }
+    ).then((data) => {
+      if (data && data.error) {
+        console.log(data.error);
+      } else {
+        auth.clearJWT(() => console.log('deleted'));
+        setRedirect(true);
+      }
+    });
+  };
+
+  const handleRequestClose = () => {
+    setOpen(false);
+  };
+
+  if (redirect) {
+    return <Redirect to="/" />;
+  }
+  return (
+    <span>
+      <IconButton aria-label="Delete" onClick={clickButton} color="secondary">
+        <DeleteIcon />
+      </IconButton>
+      <Dialog open={open} onClose={handleRequestClose}>
+        <DialogTitle>{'Delete Account'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Confirm to delete your account.</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleRequestClose} color="primary">
+            Close
+          </Button>
+          <Button
+            onClick={deleteAccount}
+            color="secondary"
+            autoFocus="autoFocus"
+          >
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </span>
+  );
+}
+
+DeleteUser.propTypes = {
+  userId: PropTypes.string.isRequired,
+};
 
 export default DeleteUser;
