28bce5bb69f91bccbd0b58ee969fd24ce109250d
Robert Finkley <rfinkley82@gmail.com>
2020-12-20 18:28:16 -0500

    Finished server side rendering logic

--- server/express.js
+++ server/express.js
@@ -33,8 +33,28 @@ app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
 app.use('/', userRoutes);
 app.use('/', authRoutes);
 
-app.get('/', (req, res) => {
-  res.status(200).send(Template());
+app.get('*', (req, res) => {
+  const sheets = new ServerStyleSheets();
+  const context = {};
+  const markup = ReactDOMServer.renderToString(
+    sheets.collect(
+      <StaticRouter location={req.url} context={context}>
+        <ThemeProvider theme={theme}>
+          <MainRouter />
+        </ThemeProvider>
+      </StaticRouter>
+    )
+  );
+  if (context.url) {
+    return res.redirect(303, context.url);
+  }
+  const css = sheets.toString();
+  res.status(200).send(
+    Template({
+      markup: markup,
+      css: css,
+    })
+  );
 });
 
 app.use((err, req, res, next) => {
