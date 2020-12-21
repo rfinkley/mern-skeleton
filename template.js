bd8ce3bea525a876cade94044dcb0e1ee5ab777c
Robert Finkley <rfinkley82@gmail.com>
2020-12-20 21:35:11 -0500

    updated template w/ ssr code

--- template.js
+++ template.js
@@ -1,4 +1,4 @@
-export default () => {
+export default ({ markup, css }) => {
   return `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -7,9 +7,16 @@ export default () => {
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
         <title>MERN Skeleton</title>
+        <style>
+            a{
+                text-decoration: none;
+                color: #061d95
+            }
+        </style>
     </head>
-    <body>
-        <div id="root">If you're reading this you're not REACTing</div>
+    <body style="margin:0">
+        <div id="root">${markup}</div>
+        <style id="jss-server-side">${css}</style>
         <script type="text/javascript" src="/dist/bundle.js"></script>
     </body>
     </html>`;
